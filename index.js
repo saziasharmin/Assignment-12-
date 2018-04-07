22bc43ce2cbd08fd1d564f6c5e82b4e579969a86
Sharmin Shoshee <saziasharmin1@gmail.com>
2018-04-07 02:32:28 +0000

    Solve the problem

--- index.js
+++ index.js
@@ -1,29 +1,18 @@
-
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
-io.emit('some event', { for: 'everyone' });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-  });
-});
-
-io.on('connection', function(socket){
-  socket.broadcast.emit('hi');
-});
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
 });
 
-
-http.listen(process.env.PORT, function(){
-  console.log('listening on *:3000');
+http.listen(port, function(){
+  console.log('listening on *:' + port);
 });
